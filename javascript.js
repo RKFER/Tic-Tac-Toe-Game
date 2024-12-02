fe7b0960c38b7beed6072be4891abc863a90383c
Rushil Khajuria <rushilkhajuria123@gmail.com>
2024-12-02 13:53:08 +0530

    javascript.js

--- javascript.js
+++ javascript.js
@@ -8,57 +8,57 @@ let turnO = true;
 let turnX = false;
 
 const winPatterns = [
-    [0,1,2],
-    [0,3,6],
-    [0,4,8],
-    [1,4,7],
-    [2,5,8],
-    [2,4,6],
-    [3,4,5],
-    [6,7,8]
+    [0, 1, 2],
+    [0, 3, 6],
+    [0, 4, 8],
+    [1, 4, 7],
+    [2, 5, 8],
+    [2, 4, 6],
+    [3, 4, 5],
+    [6, 7, 8]
 ]
 
-boxes.forEach((box)=>{
-    box.addEventListener("click",()=>{
-    if(turnO === true){
-        box.innerText = "O";
-        turnO = false;
-        turnX = true;
-    }
-        else if(turnX === true){
-        box.innerText="X";
-        turnX = false;
-        turnO = true;
+boxes.forEach((box) => {
+    box.addEventListener("click", () => {
+        if (turnO === true) {
+            box.innerText = "O";
+            turnO = false;
+            turnX = true;
         }
-    box.disabled = true;
+        else if (turnX === true) {
+            box.innerText = "X";
+            turnX = false;
+            turnO = true;
+        }
+        box.disabled = true;
 
-    checkWinner();
+        checkWinner();
     });
 });
 
-const enableBoxes =() => {
-    for(let box of boxes){
-        box.disabled= false;
-        box.innerText="";
+const enableBoxes = () => {
+    for (let box of boxes) {
+        box.disabled = false;
+        box.innerText = "";
     }
 }
 const showWinner = (winner) => {
-    win.innerText=`Winner Is ${winner}`;
+    win.innerText = `Winner Is ${winner}`;
     msg.classList.remove("hide");
     disableBoxes();
 }
 const checkWinner = () => {
-    for(let pattern of winPatterns){
-    let pos1Val = boxes[pattern[0]].innerText;
-    let pos2Val = boxes[pattern[1]].innerText;
-    let pos3Val = boxes[pattern[2]].innerText;
+    for (let pattern of winPatterns) {
+        let pos1Val = boxes[pattern[0]].innerText;
+        let pos2Val = boxes[pattern[1]].innerText;
+        let pos3Val = boxes[pattern[2]].innerText;
 
-    if(pos1Val != "" && pos2Val != ""&& pos3Val != ""){
-        if(pos1Val===pos2Val && pos2Val===pos3Val){
-            console.log("Winner",pos1Val);
-            showWinner(pos1Val);
+        if (pos1Val != "" && pos2Val != "" && pos3Val != "") {
+            if (pos1Val === pos2Val && pos2Val === pos3Val) {
+                console.log("Winner", pos1Val);
+                showWinner(pos1Val);
+            }
         }
-    } 
     }
 };
 const resetGame = () => {
@@ -72,5 +72,5 @@ const newGame = () => {
     enableBoxes();
     msg.classList.add("hide");
 }
-newGme.addEventListener("click",newGame);
-resetBtn.addEventListener("click",resetGame);
+newGme.addEventListener("click", newGame);
+resetBtn.addEventListener("click", resetGame);
